77e5542b44e8e65f05ab880d63d15e263827403a
yex177 <2099063209@qq.com>
2026-03-29 15:33:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/', (req, res) => {
 app.use(errorHandler);
 
 // 启动服务
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
